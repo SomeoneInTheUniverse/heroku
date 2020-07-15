f49933000304dad43e1589e8c8b66a99f6edbd3e
someone in the universe <63373211+SomeoneInTheUniverse@users.noreply.github.com>
2020-07-15 16:21:36 +1000

    Update index.js
    Fixed it,

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Discord = require("discord.js");
  DISCORD.JS VERSION 12 CODE
 */
 
-
+node bot.js
 // This is your client. Some people call it `bot`, some people call it `self`, 
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
 // this is what we're refering to. Your client.
